9c665e0a6a6a1d1ba9115971463a60eef604174c
Yann Plougonven <plougonven-y@saint-louis29.net>
2022-09-13 11:18:22 +0200

    tkt

--- src/Films/cinema.py
+++ src/Films/cinema.py
@@ -1,35 +1,60 @@
 class Realisateur:
-    def __init__(self, ID, nom, prenom):
-        self._ID = ID
+    def __init__(self, identifiant, nom, prenom):
+        self._identifiant = identifiant
         self._nom = nom
         self._prenom = prenom
+        self._films = []
         
-    def get_ID:
-        return self._ID
+    def get_identifiant(self):
+        return self._identifiant
     
-    def get_nom:
+    def get_nom(self):
         return self._nom
     
-    def get_prenom:
+    def get_prenom(self):
         return self._prenom
     
+    def presentation(self):
+        return f"realisateur : {self._prenom} {self._nom}"
     
-class Film:
-    def __init__(self, ID, titre, annee):
-        self._ID = ID
+    def ajouter_film(self, film):
+        self._films.append(film)
+        film._realisateur = self
+        
+    def get_films(self):
+        return self._films
+    
+    
+class Film():
+    def __init__(self, identifiant, titre, annee):
+        self._identifiant = identifiant
         self._titre = titre
         self._annee = annee
+        self._realisateur = None
         
-    def get_ID:
-        return self._ID
+    def get_identifiant(self):
+        return self._identifiant
     
-    def get_titre:
+    def get_titre(self):
         return self._titre
     
-    def get_annee:
+    def get_annee(self):
         return self._annee
-        
     
-        
+    def presentation(self):
+        return self.get_titre()
     
+    def __repr__(self):
+        return self.presentation()
     
+
+r1 = Realisateur(1, "Donald", "Trump")
+#print(r1.presentation())
+f1 = Film(1, "Le roi Yann", 2005)
+f2 = Film(2, "apprends l'alphabet avec Dora", 2002)
+#print("film :", f1.presentation())
+
+r1.ajouter_film(f1)
+r1.ajouter_film(f2)
+
+print(r1.get_films())
