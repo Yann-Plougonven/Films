44e1ef14c121a28497d5837fc8c18189c9ee2556
Yann PLOUGONVEN--LASTENNET <y.plougonven@workstation4.nsi.lan>
2022-09-13 10:28:13 +0200

    rien

---
+++ src/Films/cinema.py
@@ -0,0 +1,35 @@
+class Realisateur:
+    def __init__(self, ID, nom, prenom):
+        self._ID = ID
+        self._nom = nom
+        self._prenom = prenom
+        
+    def get_ID:
+        return self._ID
+    
+    def get_nom:
+        return self._nom
+    
+    def get_prenom:
+        return self._prenom
+    
+    
+class Film:
+    def __init__(self, ID, titre, annee):
+        self._ID = ID
+        self._titre = titre
+        self._annee = annee
+        
+    def get_ID:
+        return self._ID
+    
+    def get_titre:
+        return self._titre
+    
+    def get_annee:
+        return self._annee
+        
+    
+        
+    
+    
